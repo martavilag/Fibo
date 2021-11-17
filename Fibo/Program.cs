4f2720fe0580047505159d7b1b4793c0405a4ad5
mvila <mvila@voxelgroup.net>
2021-11-17 10:42:10 +0100

    Add project files.

---
+++ Fibo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Fibo
+{
+    class Program
+    {
+        static int Sequence(int number)
+        {
+            int a = 0;
+            int b = 1;
+            int i = 0;
+            int final = int.Parse(Console.ReadLine());
+
+            while (int i = 0; i < final; i++) 
+        }
+
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
